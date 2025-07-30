8fde9f84583b2aa42134161a4d72318773313fb5
Serhat Kenar <serhat.kenar@akinon.com>
2025-07-30 18:05:59 +0300

    main

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -250,7 +250,7 @@ const Header = ({ title, user, className }: HeaderProps) => {
   
   const navigation: NavigationItem[] = [
     {
-      name: 'Dashboard',
+      name: 'Anasayfa',
       href: '/dashboard',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
